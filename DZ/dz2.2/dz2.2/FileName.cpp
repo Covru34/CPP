6aad81f80696b3613735097aabd2e4755ee78518
Covru34 <mikolamihalcuk01@gmail.com>
2026-03-16 11:42:46 +0200

    did dz2.3

---
+++ DZ/dz2.2/dz2.2/FileName.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<Windows.h>
+
+int main()
+{
+	using namespace std;
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+
+	cout << "\tПродам цуценят.\n";
+	cout << "\tПорода: ротвейлерю\n\n";
+	cout << "\t...\n";
+	cout << "\tТел.:222-22-22\n";
+	cout << "\t222     | 222 |\n";
+	cout << "\t22      | 22  |\n";
+	cout << "\t22      | 22  |\n";
+
+	return 0;
+}

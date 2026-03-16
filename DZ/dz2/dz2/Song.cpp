ea8c48b5959c3b5d9ff0a371e7561b987f4220df
Covru34 <mikolamihalcuk01@gmail.com>
2026-03-16 11:32:25 +0200

    did dz2

---
+++ DZ/dz2/dz2/Song.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+
+	cout << "Ay, ay, ayy, I'm on vacation\n";
+	cout << "Every single day 'cause I love my occupation\n";
+	cout << "Ay, ay, ayy, I'm on vacation\n";
+	cout << "If you don't like your life, then\n";
+	cout << "You should go and change it\n";
+	cout << "Ay, ay, ayy, I'm on vacation\n";
+	cout << "Every singe day 'cause I love my occupation\n";
+	cout << "Ay, ay, ayy, I'm on vacation\n";
+	cout << "Every single day, every, every single day\n";
+	cout << "Every single day, every, every single day\n";
+	cout << "Every single day, every, every single day-ay\n";
+
+	return 0;
+}

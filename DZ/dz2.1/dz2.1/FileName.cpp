5263c4c14a9322f62f2e52f42a44378c05f35c21
Covru34 <mikolamihalcuk01@gmail.com>
2026-03-16 11:34:48 +0200

    did dz2.1

---
+++ DZ/dz2.1/dz2.1/FileName.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Every hunter wants\n";
+    cout << "\tto know\n";
+    cout << "\t\twhere sits\n";
+    cout << "\t\t\tthe pheasant\n";
+
+    return 0;
+}

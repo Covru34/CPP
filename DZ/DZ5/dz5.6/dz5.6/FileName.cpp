2050c18b42963a433e293b1b5a0dc5c2641191f0
Covru34 <mikolamihalcuk01@gmail.com>
2026-03-28 10:53:13 +0200

    did dz5.6

---
+++ DZ/DZ5/dz5.6/dz5.6/FileName.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+	
+	for (int i = 100;i <= 999; i++)
+	{
+		int number1 = i / 100;
+		int number2 = i / 10 % 10;
+		int number3 = i % 10;
+
+		if (number1 != number2 && number2 != number3 && number3 != number1)
+		{
+			cout << i << " ";
+		}
+	}
+
+		
+	return 0;
+}

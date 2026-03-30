e3b72917be9851db64a015da4c25c7b2f64725ce
Covru34 <mikolamihalcuk01@gmail.com>
2026-03-30 16:02:49 +0300

    did dz5.8

--- DZ/DZ5/dz5.8/dz5.8/FileName.cpp
+++ DZ/DZ5/dz5.8/dz5.8/FileName.cpp
@@ -7,9 +7,24 @@ int main()
 	SetConsoleOutputCP(65001);
 	SetConsoleCP(65001);
 
+	bool found = false;
 	int A;
 	cout << "Введіть ціле число: \n";
 	cin >> A;
+
+	for (int B = 1; B * B <= A; B++)
+	{
+		if (A % (B * B) == 0 && A % (B * B * B) != 0)
+		{
+			cout << B << " ";
+			found = true;
+		}
+	}
+
+	if (!found)
+	{
+		cout << "Немає таких B";
+	}
 
 	return 0;
 }

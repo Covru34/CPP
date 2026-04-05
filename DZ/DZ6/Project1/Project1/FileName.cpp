45ffdfb77df116a2e8dbf64341d6e73b9c7a8c6d
Covru34 <mikolamihalcuk01@gmail.com>
2026-04-05 15:29:26 +0300

    did dz6

---
+++ DZ/DZ6/Project1/Project1/FileName.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+int main()
+{
+	using namespace std;
+	cout << "What's up, Doc! \n ";  //приклад 1, с. 29
+	//return 0;
+
+
+//#include<iostream>
+//int main()
+
+using namespace std;
+cout << " Come up and С++ me some time.";
+cout << endl;								// приклад 2, с.50
+cout << "Уои won't regret it  " << endl;
+//return 0;
+	
+//#include<iostream>
+//int main()
+	
+using namespace std;
+int carrots;
+cout << "How many carrots do you have ?" << endl;
+cin >> carrots;										// приклад 3, с. 67
+cout << "Here are two more.";
+carrots = carrots + 2;
+	
+cout << "Now you have " << carrots << " carrots." << endl;
+return 0;
+}

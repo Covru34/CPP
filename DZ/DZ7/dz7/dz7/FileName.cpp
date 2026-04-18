09a1f80a6abd5b2be56e8ff7cb075c86bb4b361c
Mykola <mikolamihalcuk01@gmail.com>
2026-04-18 21:17:18 +0300

    did dz7

---
+++ DZ/DZ7/dz7/dz7/FileName.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<Windows.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+
+	int True[3] = {};
+	int user[3] = {};
+
+	return 0;
+}

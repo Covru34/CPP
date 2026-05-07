3e0146be9250d25d19af74241968fcae1da3923d
Mykola <mikolamihalcuk01@gmail.com>
2026-05-07 17:16:56 +0300

    did dz10.1

---
+++ DZ/DZ10/dz10.1/dz10.1/FileName.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+double X(int arr[], int size);
+
+int main()
+{
+	SetConsoleOutputCP(65001);
+	SetConsoleCP(65001);
+
+	int size = 0;
+	
+	cout << "Введіть розмір масива: \n";
+	cin >> size;
+	int arr[100];
+	cout << "Введіть елементи масиву: \n";
+	for (int i = 0; i < size; i++)
+	{
+		cin >> arr[i];
+	}
+	
+	cout << "Середнє арифметичне: " << X(arr, size) << endl;
+
+	return 0;
+}
+
+double X(int arr[], int size)
+{
+	if (size == 0)
+		return 0.0;
+	double sum = 0;
+	for (int i = 0; i < size; i++)
+	{
+		sum += arr[i];
+	}
+	return sum / size;
+}

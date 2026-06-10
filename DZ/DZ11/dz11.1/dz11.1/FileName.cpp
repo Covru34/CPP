a8ad4644acd5ae967087b6619d35d6532a3e7ac2
Mykola <mikolamihalcuk01@gmail.com>
2026-06-10 12:04:41 +0300

    did dz11.1

--- DZ/DZ11/dz11.1/dz11.1/FileName.cpp
+++ DZ/DZ11/dz11.1/dz11.1/FileName.cpp
@@ -7,7 +7,29 @@ int main()
 	SetConsoleOutputCP(65001);
 	SetConsoleCP(65001);
 
+	int arr[5] = { 10, 20, 30, 40, 50 };
+	int* left = arr;
+	int* right = arr + 5 - 1;
 
+	for (int i = 0; i < 5;i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+
+	while (left < right)
+	{
+		int tmp = *left;  // зберігаємо значення лівого
+		*left = *right; // на місці лівого записуємо значення правого
+		*right = tmp; // на місці правого записуємо збережене значення лівого - tmp
+		left++;
+		right--;
+	}
+
+	for (int i = 0; i < 5;i++)
+	{
+		cout << arr[i] << "\t";
+	}
 
 	return 0;
 }

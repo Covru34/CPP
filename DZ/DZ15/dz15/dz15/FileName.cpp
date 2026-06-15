68ed642727a864eec66b7dd0dc6e4579f83803c6
Mykola <mikolamihalcuk01@gmail.com>
2026-06-15 16:47:24 +0300

    burmalda

--- DZ/DZ15/dz15/dz15/FileName.cpp
+++ DZ/DZ15/dz15/dz15/FileName.cpp
@@ -7,6 +7,11 @@ int main()
 	SetConsoleOutputCP(65001);
 	SetConsoleCP(65001);
 
-
+	int rows, cols;
+	int** arr = new int* [rows];
+	for (int i = 0; i < rows; i++)
+	{
+		arr[i] = new int[cols];
+	}
 
 }

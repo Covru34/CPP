a24414af9c58243701868f0dd32b5fe5f07f15a7
Mykola <mikolamihalcuk01@gmail.com>
2026-06-20 12:07:51 +0300

    commit

--- DZ/DZ15/dz15/dz15/FileName.cpp
+++ DZ/DZ15/dz15/dz15/FileName.cpp
@@ -7,11 +7,8 @@ int main()
 	SetConsoleOutputCP(65001);
 	SetConsoleCP(65001);
 
-	int rows, cols;
-	int** arr = new int* [rows];
-	for (int i = 0; i < rows; i++)
-	{
-		arr[i] = new int[cols];
-	}
+	int rows = 0, cols = 0;
+	int arr[rows][cols];
+	
 
 }
--- LYPA PYPA/Project1/Project1/FileName.cpp
+++ LYPA PYPA/Project1/Project1/FileName.cpp
@@ -4,8 +4,13 @@
 #include <cstring>
 using namespace std;
 
+const int N = 6;
+const int n = 4, m = 2;
+
 void printArr(string arr[], int N);
 void inputArr(string arr[], int N);
+void inputDoubleArr(string arr[][m], int n);
+void printDoubleArr(string arr[][m], int n);
 
 int main()
 {
@@ -13,13 +18,18 @@ int main()
 	SetConsoleCP(65001);
 
 	cout << "Робота з масивами про поні🤣😂🤣😂🤣\n\n";
-	const int N = 6;
+	
+	string DoubleMLP[n][m];
 	string MLP[N];// = { "Twilight Sparkle", "Pinkie Pie", "AppleJack", "Fluttershy", "Rainbow Dash", "Rarity" };
 
-	cout << "Введіть головну шестірку: \n";
-	inputArr(MLP, N);
-	cout << "***Головна шестірка***\n";
-	printArr(MLP, N);
+	//cout << "Введіть головну шестірку: \n";
+	//inputArr(MLP, N);
+	//cout << "\n***Головна шестірка***\n";
+	//printArr(MLP, N);
+	cout << "\n***Розподіліть поні по їхнім расам***\n";
+	inputDoubleArr(DoubleMLP, n);
+	cout << "\n***Результат***\n";
+	printDoubleArr(DoubleMLP, n);
 
 	return 0;
 }
@@ -40,5 +50,29 @@ void inputArr(string arr[], int N)
 	{
 		cout << "Поні " << i + 1 << ": ";
 		getline(cin, arr[i]);
+	}
+}
+
+void inputDoubleArr(string arr[][m], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << "Елемент [" << i << "][" << j << "]: ";
+			getline(cin, arr[i][j]);
+		}
+	}
+}
+
+void printDoubleArr(string arr[][m], int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			cout << arr[i][j] << "\t";
+		}
+		cout << "\n";
 	}
 }

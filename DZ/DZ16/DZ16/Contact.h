0b3fd14fb126b35f08cef1c5012902ada5183277
Covru34 <mikolamihalcuk01@gmail.com>
2026-08-02 10:28:16 +0300

    commit

--- DZ/DZ16/DZ16/Contact.h
+++ DZ/DZ16/DZ16/Contact.h
@@ -3,16 +3,3 @@
 #include <string>
 #include <fstream>
 using namespace std;
-
-class Contact
-{
-private:
-	char* fullName;
-	string homePhone; 
-	string workPhone;
-	string mobilePhone;
-	string additionalInfo;
-public:
-	char* fullName = nullptr;
-};
-
--- DZ/DZ16/DZ16/FileName.cpp
+++ DZ/DZ16/DZ16/FileName.cpp
@@ -1,12 +1,43 @@
 #include<iostream>
 #include<Windows.h>
 using namespace std;
- 
+
+class Hero {
+private:
+	string name;
+	int health;
+public:
+	Hero(string heroName, int heroHealth) {
+		name = heroName;
+		health = heroHealth;
+	}
+
+	int getHealth() {
+		return health;
+	}
+
+	string getName() {
+		return name;
+	}
+
+	int takeDamage(int damage) {
+		health -= damage;
+		if (health < 0)
+			health = 0;
+		return health;
+	}
+
+	
+};
+
 int main()
 {
 	SetConsoleOutputCP(65001);
 	SetConsoleCP(65001);
 
+	Hero lupik("Люпік", 100);
+
+	lupik.takeDamage(120);
 
 	return 0;
 }

533e22fd8126aed76fca34441aac7a39f9552416
Covru34 <mikolamihalcuk01@gmail.com>
2026-08-17 20:00:16 +0300

    working

--- DZ/DZ17/DZ17/FileName.cpp
+++ DZ/DZ17/DZ17/FileName.cpp
@@ -1,6 +1,24 @@
 #include<iostream>
 #include<Windows.h>
+#include<string>
 using namespace std;
+
+class Car
+{
+private:
+	string carNumber; // номер автомобілля 
+	string* fines;// список штрафів
+	int finesCount; // кількістиь штрафів
+public:
+	Car(string number = "")
+	{
+		carNumber = number;
+		finesCount = 0;
+		fines = nullptr;
+	}
+
+
+};
 
 int main()
 {

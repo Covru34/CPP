1b73ef9351ae65665f9dbc04b1a0a3c322106355
Covru34 <mikolamihalcuk01@gmail.com>
2026-08-21 12:05:53 +0300

    add class Workout and updated class Athlete

--- Other/TASK/task15/task15/FileName.cpp
+++ Other/TASK/task15/task15/FileName.cpp
@@ -9,17 +9,19 @@ private:
 	string name;
 	int benchPress;
 	int squats;
+	int deadlift;
 public:
-	Athlete(string name, int benchPress, int squats)
+	Athlete(string name, int benchPress, int squats, int deadlift)
 	{
 		this->name = name;
 		this->benchPress = benchPress;
 		this->squats = squats;
+		this->deadlift = deadlift;
 	}
 
 	void show()
 	{
-		cout << "Спортсмен: " << name << " | Жим: " << benchPress << " кг | Присяд: " << squats << " кг\n";
+		cout << "Спортсмен: " << name << " | Жим: " << benchPress << " кг | Присяд: " << squats << " кг | Станова: " << deadlift << " кг\n";
 	}
 
 	void addBPweight(int weight)
@@ -32,7 +34,56 @@ public:
 		squats += weight;
 	}
 
+	void addDeadlift(int weight)
+	{
+		deadlift += weight;
+	}
 
+	int getTotal() const
+	{
+		return benchPress + squats + deadlift;
+	}
+
+	string getName()
+	{
+		return name;
+	}
+
+	bool operator>(const Athlete& other) const
+	{
+		return this->getTotal() > other.getTotal();
+	}
+
+	friend ostream& operator<<(ostream& os, const Athlete& ath)
+	{
+		os << "Спортсмен: " << ath.name
+			<< " | Жим: " << ath.benchPress
+			<< " кг | Присяд: " << ath.squats
+			<< " кг | Станова: " << ath.deadlift
+			<< " кг\n";
+		return os;
+	}
+
+};
+
+class Workout
+{
+private:
+	Athlete& athlete;
+	string data;
+public:
+	Workout(Athlete& ath, string d) : athlete(ath), data(d) {};
+
+	void startSession()
+	{
+		cout << "--- Тренування за " << data << " ---\nПочатковий стан спротсмена: ";
+		cout << athlete;
+		athlete.addBPweight(1);
+		athlete.addSweight(1);
+		athlete.addDeadlift(1);
+		cout << "Стан спортсиена після тренування:\n";
+		cout << athlete;
+	}
 };
 
 int main()
@@ -40,15 +91,25 @@ int main()
 	SetConsoleOutputCP(65001);
 	SetConsoleCP(65001);
 
-	Athlete Pablin("Паблін", 120, 190);
-	Pablin.show();
-	Pablin.addBPweight(10);
-	Pablin.show();
+	Athlete Pablin("Паблін", 120, 190, 250);
+	cout << Pablin;
+	
+	Athlete BortProvodnik("Борт", 90, 140, 170);
+	cout << BortProvodnik;
+	
+	//if (Pablin > BortProvodnik)
+	//{
+	//	cout << Pablin.getName() << " сильніший за " << BortProvodnik.getName() << '\n';
+	//}
+	//else {
+	//	cout << BortProvodnik.getName() << " сильніший за " << Pablin.getName() << '\n';
+	//}
 
-	Athlete BortProvodnik("Борт", 90, 140);
-	BortProvodnik.show();
-	BortProvodnik.addSweight(10);
-	BortProvodnik.show();
+	Workout p(Pablin, "21.08.2026");
+	p.startSession();
+
+	cout << "\nПеревірка в main після тренування:\n";
+	cout << Pablin;
 
 	return 0;
 }
